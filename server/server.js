5e0809673ede8f0ae77525993a311c6915407095
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-08-31 13:40:08 +1000

    Configure server to serve production build. Configure app to use relative API url.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const apiRouter = require("./controllers/api")
 const app = express() 
 app.use(cors())
 app.use(express.json()) 
+app.use(express.static('build'))
 app.use(apiRouter)
 
 const PORT = 3001
--- src/services/units.js
+++ src/services/units.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = "http://localhost:3001/api/"
+const baseURL = "/api/"
 
 /**
  * Get a list of all unitsfrom the api
